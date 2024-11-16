4078f435f1839e8dd35a9ae65b8d88a337f6ed8d
Aman Kumar Singh <aman20012004@gmail.com>
2024-11-16 21:04:34 +0530

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// Define public routes
 const isPublicRoute = createRouteMatcher(["/site", "/api/uploadthing"]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // rewrite for domains
   const url = req.nextUrl;
   const searchParams = url.searchParams.toString();
-  const hostname = req.headers;
+  const hostname = req.headers.get("host");
 
   const pathWithSearchParams = `${url.pathname}${
     searchParams.length > 0 ? `?${searchParams}` : ""
   }`;
 
-  // Allow access to public routes without authentication
   if (isPublicRoute(req)) {
     return NextResponse.next();
   }
 
-  // Rewrite based on domain logic
-  const customSubDomain = hostname
-    .get("host")
-    ?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`)
-    .filter(Boolean)[0];
+  const customSubDomain = hostname && process.env.NEXT_PUBLIC_DOMAIN
+  ? hostname.split(process.env.NEXT_PUBLIC_DOMAIN).filter(Boolean)[0]
+  : undefined;
 
   if (customSubDomain) {
     return NextResponse.rewrite(
@@ -31,32 +26,27 @@ export default clerkMiddleware(async (auth, req) => {
     );
   }
 
-  // Redirect to /agency/sign-in for sign-in and sign-up paths
   if (url.pathname === "/sign-in" || url.pathname === "/sign-up") {
     return NextResponse.redirect(new URL(`/agency/sign-in`, req.url));
   }
 
-  // Rewriting to /site
   if (
     url.pathname === "/" ||
-    (url.pathname === "/site" && url.host === process.env.NEXT_PUBLIC_DOMAIN)
+    (url.pathname === "/site" && hostname === process.env.NEXT_PUBLIC_DOMAIN)
   ) {
-    console.log("Rewriting '/' to '/site'");
     return NextResponse.rewrite(new URL("/site", req.url));
   }
 
-  // Rewrite /agency and /subaccount paths to absolute URLs
   if (
-    url.pathname.startsWith("/agency") ||
-    url.pathname.startsWith("/subaccount")
+    url.pathname.startsWith('/agency') ||
+    url.pathname.startsWith('/subaccount')
   ) {
-    return NextResponse.rewrite(new URL(`/${pathWithSearchParams}`, req.url));
+    return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
   }
 
-  // Protect non-public routes
   await auth.protect();
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
